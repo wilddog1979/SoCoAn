d15e5a4e21fb74bd69d16a692a1517de4e17c435
wilddog <wilddog@192.168.14.22>
2016-09-25 21:29:02 +0200

    issue fixed with Java8

---
+++ Core/src/org/eaSTars/socoan/lang/package-info.java
@@ -0,0 +1,2 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://eaSTars.com/SoCoAn/lang", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package org.eaSTars.socoan.lang;
